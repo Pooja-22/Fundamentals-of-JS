c287b24fe7b7e5a35a8285a5473ae01204271ace
Pooja-22 <pooja.garg@tothenew.com>
2016-03-27 15:21:54 +0530

    Using New Keyword

--- Inheritance/Inheritance.js
+++ Inheritance/Inheritance.js
@@ -11,20 +11,33 @@ var obj2 = {
     address : "1",
     college : "2"
 };
-/*
-now __proto__ of obj1 contains the properties defined in obj2, so we can access the properties of obj2 in obj1 See line 29
+
+/**
+ * now __proto__ of obj1 contains the properties defined in obj2, so we can access the properties of obj2 in obj1 See line 29
  */
 
 obj1.__proto__ = obj2;
 
 console.log("object", obj1.address);
 
-/*
-Obj3 now contains all the properties of obj1.
+/**
+ * Obj3 now contains all the properties of obj1.
+ * @type {obj1}
  */
 
 var obj3 = Object.create(obj1);
+
 obj3.newProperty  = "Inheritance";
+
 console.log("new Object",obj3.firstName,obj3.address,obj3.newProperty);
 
+var trying = function(){
+    this.name = "pooja";
+    trying.prototype.print = function (){
+        console.log("hey");
+    }
+};
+var newClass = new trying();
 
+console.log(newClass.name);
+newClass.print();
